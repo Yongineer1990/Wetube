01e6313601c5aac0a195ba27bb2c9f65d03ec398
yongineer1990@gmail.com <60580491+Yongineer1990@users.noreply.github.com>
2020-03-17 02:43:28 +0900

    postJoin fixed

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -6,7 +6,7 @@ import { getJoin, login, logout, postJoin } from "../controllers/userController"
 const globalRouter = express.Router();
 
 globalRouter.get(routes.join, getJoin);
-globalRouter.get(routes.join, postJoin);
+globalRouter.post(routes.join, postJoin);
 
 
 globalRouter.get(routes.home, home);
