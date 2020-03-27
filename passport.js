41fdc8ae3ca7e4a8453a8ba2e6e96e4f6f3e79d7
yongineer1990@gmail.com <60580491+Yongineer1990@users.noreply.github.com>
2020-03-28 01:12:38 +0900

    #7.0

--- passport.js
+++ passport.js
@@ -26,7 +26,9 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `http://localhost:4000${routes.facebookCallback}`
+      callbackURL: `https://1762b42e.ngrok.io${routes.facebookCallback}`,
+      profileFields: ["id", "displayName", "photos", "email"],
+      scope: ["public_profile", "email"]
     },
     facebookLoginCallback
   )
