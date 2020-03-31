9b8cb38523194e3a676115520cf018180d80499f
yongineer1990@gmail.com <60580491+Yongineer1990@users.noreply.github.com>
2020-04-01 00:06:38 +0900

    #8.0 4:04

--- models/User.js
+++ models/User.js
@@ -6,7 +6,19 @@ const Userschema = new mongoose.Schema({
   email: String,
   avatarUrl: String,
   facebookId: Number,
-  githubId: Number
+  githubId: Number,
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ],
+  videos: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video"
+    }
+  ]
 });
 
 Userschema.plugin(passportLocalMongoose, { usernameField: "email" });
